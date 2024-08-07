9c649e050cbd6d0081341f071670c7ab7d40d254
ZagZx <zagzcraft@gmail.com>
2024-08-06 21:22:45 -0300

    testando gramas

--- main.py
+++ main.py
@@ -5,26 +5,43 @@ import time
 
 pygame.init()
 
-tela = pygame.display.set_mode((600,600))
+tela = pygame.display.set_mode((700,700))
 
+
+#CARREGANDO IMAGENS v
 knight = pygame.image.load('imagens/Knight.png')
 knight = pygame.transform.scale(knight,(48,48))
 
-princess = pygame.image.load('imagens/Princesa.png')
-princess = pygame.transform.scale(princess,(48,48))
+princesa = pygame.image.load('imagens/Princesa.png')
+princesa = pygame.transform.scale(princesa,(48,48))
+
+grama = pygame.image.load('imagens/grama64x64.png')
+grama = pygame.transform.scale(grama,(48,48))
+#CARREGANDO IMAGENS ^
+
 #knight = pygame.transform.flip(knight,) #isso vai ser para virar o personagem ao trocar de lado
 
 
-#APRENDER A USAR DRAW v
-#teste = pygame.draw.line(tela,'white', 10,480,5)
-#APRENDER A USAR DRAW ^
+
+#teste = pygame.draw.line(tela,'white',10,148,5)
+
 run = True
 while run:
+    for a in range(0,10):
+        for b in range(0,10):
+            tela.blit(grama,(148+48*a,148+48*b))
     tela.blit(knight,(148,148))
-    tela.blit(princess,(148+48,148))
+    tela.blit(princesa,(148+48,148))
+
+    #EVENTOS v
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        
+    #EVENTOS ^
+
+
+
     pygame.display.flip()
 pygame.quit()
 
