9329fa79903efd976e0e7381d6e54a9ab2386be1
ZagZx <zagzcraft@gmail.com>
2024-08-07 21:27:48 -0300

    algumas burrices corrigidas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randint
 import time
+import sys
 
 
 pygame.init()
@@ -15,10 +16,10 @@ knight = pygame.transform.scale(knight,(48,48),)
 princesa = pygame.image.load('imagens/Princesa.png')
 princesa = pygame.transform.scale(princesa,(48,48))
 
-grama = pygame.image.load('imagens/grama64x64.png')
+grama = pygame.image.load('imagens/Grama.png')
 grama = pygame.transform.scale(grama,(48,48))
 
-barreira = pygame.image.load('imagens/preda.png.png')
+barreira = pygame.image.load('imagens/Preda.png')
 barreira = pygame.transform.scale(barreira,(48,48))
 
 grid = pygame.image.load('imagens/Mapa.png')
@@ -35,10 +36,21 @@ grid = pygame.transform.scale(grid,(480,480))
 
 run = True
 while run:
+    #EVENTOS v
+    
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            
+            run = False
+
+    #EVENTOS ^
+
+    tela.fill('gray')
 
     for a in range(0,10):
         for b in range(0,10):
-            tela.blit(grama,(110+48*a,110+48*b))
+            tela.blit(grama,(112+48*a,112+48*b))
     
     for a in range(0,12):
 
@@ -48,23 +60,15 @@ while run:
         tela.blit(barreira,(64,64+48*a))
 
 
-    tela.blit(grid,(110,110))
+    tela.blit(grid,(112,112))
 
-    # tela.blit(knight,(110,110))
-    tela.blit(princesa,(156,156))
+    tela.blit(knight,(112,112))
+    tela.blit(princesa,(160,160))
     #tela.blit(knight,krect)
 
-    #EVENTOS v
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-        # if event.type == pygame.K_w:
-        #     pass
-        
-    #EVENTOS ^
+    
     
     pygame.display.flip()
 pygame.quit()
-
+sys.exit()
 
