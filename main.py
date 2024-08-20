68900ec607eb96cda62468ae388eec25144641ee
Joyuv <givanilsonmariz7@outlook.com>
2024-08-20 09:58:06 -0300

    Revert "poggers"
    This reverts commit 5e088421d642c9a3a07e7bfa0b74b522fecd7f43.
    
    noggers

--- main.py
+++ main.py
@@ -128,11 +128,11 @@ def jogar():
         
         def andar(self,charx, chary):
             
-            if self.__coorx != charx and self.__coory != chary:
+            if self.__coorx != charx or self.__coory != chary:
                 
                 self.random = bool(getrandbits(1))
 
-                if self.random and self.__coorx != charx:
+                if self.random:
                     if self.__coorx < charx:
                         self.__coorx += 48
                     elif self.__coorx > charx:
@@ -308,13 +308,12 @@ def jogar():
     vida_inicial = jgdr1.get_vida()
     
     vigorinicial = jgdr1.get_stamina()
-
-    charect = pygame.Rect(jgdr1.get_coorx(),jgdr1.get_coory(),48,48)
+    
     while run:
         
         
         
-        
+        charect = pygame.Rect(jgdr1.get_coorx(),jgdr1.get_coory(),48,48)
         #region EVENTOS
         
         for event in pygame.event.get():
@@ -347,17 +346,16 @@ def jogar():
                         explosao = True
                 
         #endregion EVENTOS
-        charect = pygame.Rect(jgdr1.get_coorx(),jgdr1.get_coory(),48,48)
-
         if charect.colliderect(prinrect):
             run = False
             ganhou = True
+            print('princesa')
             
 
         elif jgdr1.get_vida() <= 0 or jgdr1.get_stamina() <= 0:
             run = False
             perdeu = True
-        
+            print('fodeu')
         
 
         tela.fill('black')
@@ -424,7 +422,7 @@ def jogar():
                     case 4:
                         fliphor =pygame.transform.rotate(exlados,90)
                         tela.blit(fliphor,(exrects[a]))
-            
+
                 if exrects[a].colliderect(prinrect):       
                     decapitado = True
                     run = False
